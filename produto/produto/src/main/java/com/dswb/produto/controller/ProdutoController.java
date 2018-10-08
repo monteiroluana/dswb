6cb71b834da1a9ead004bd426b5f6fd2de8db152
Luana Monteiro <monteiroluana@scicrop.com>
2018-10-08 10:45:02 -0300

    teste method POST

--- produto/produto/src/main/java/com/dswb/produto/controller/ProdutoController.java
+++ produto/produto/src/main/java/com/dswb/produto/controller/ProdutoController.java
@@ -6,9 +6,13 @@ import java.sql.SQLException;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -28,4 +32,30 @@ public class ProdutoController {
         Produto produto = service.pegarProduto(id);
         return new ModelAndView("mostrarProduto").addObject("produto", produto);
     }
+    
+    
+    @RequestMapping(value="/getProduto", method=RequestMethod.POST, params="action=edit")
+    public ModelAndView editar(@ModelAttribute("produto")Produto produto) throws SQLException {
+    	ProdutoService service = new ProdutoService();
+    	produto = service.pegarProduto(produto.getId());
+    	return new ModelAndView("mostrarProduto").addObject("produto", produto);
+    }
+    
+       
+    
+   /* @PostMapping("/getProduto")
+    public ModelAndView salvar(@ModelAttribute("produto") Produto produto, 
+            RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("produto", produto);
+        ModelAndView retorno = new ModelAndView("redirect:/produto/resultado");
+        return retorno;
+    }
+    
+    @GetMapping("/resultado")
+    public ModelAndView mostrarResultado() {
+        return new ModelAndView("pessoa/resultado");
+    }*/
+    
+    
 }
